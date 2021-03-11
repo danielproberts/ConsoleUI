d483af9cccbc519c18e676136f4f0c5bc2927486
Daniel Roberts <daniel.prescott.roberts@gmail.com>
2021-03-11 09:54:07 -0500

    Add project files.

---
+++ ConsoleUI/ConUI.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    class ConUI
+    {
+        public ConUI()
+        {
+
+        }
+
+        public static void CenterTitle(string s)
+        {
+            string windowHeader = s;
+            Console.SetCursorPosition((Console.WindowWidth - windowHeader.Length) / 2, Console.CursorTop);
+            Console.WriteLine(windowHeader);
+        }
+
+        public static void GenerateListMenu(string[] menuOptions)
+        {
+            var currentItem = 1;
+            foreach (var item in menuOptions)
+            {
+                Console.Write($"{currentItem}) ");
+                Console.WriteLine(item);
+                currentItem+=1;
+            }
+        }
+
+        public static void GenerateBarMenu(string[] menuOptions)
+        {
+            var menuLength = 0;
+            var menuOutput = "";
+            foreach(var item in menuOptions)
+            {
+                menuLength += item.Length;
+            }
+
+
+
+            var numOfItems = menuOptions.Length;
+            //Console.SetCursorPosition((Console.WindowWidth) / numOfItems, Console.CursorTop);
+            foreach(var item in menuOptions)
+            {
+                menuOutput += item;
+                menuOutput = menuOutput.PadRight(menuOutput.Length + (menuLength/numOfItems));
+            }
+            Console.WriteLine(menuOutput);
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] menuItems = { "File", "Edit", "View", "Project", "Build", "Debug", "Test", "Analyze", "Tools", "Window", "Help" };
+
+
+            ConUI.CenterTitle("Sample Menu");
+
+            ConUI.GenerateBarMenu(menuItems);
+            Console.ReadKey();
+        }
+    }
+}
